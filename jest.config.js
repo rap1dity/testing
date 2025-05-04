fa48161e134bdcb6b4208239904485f3fec8c4dd
Andrew <Konwertik1@mail.ru>
2025-05-04 17:38:03 +0200

    chore(L-2): Added babel, eslint, husky configs

--- jest.config.js
+++ jest.config.js
@@ -6,15 +6,15 @@ module.exports = {
       pageTitle: 'Test Report',
       outputPath: 'report/test-report.html',
       includeFailureMsg: true,
-      includeConsoleLog: true
-    }]
+      includeConsoleLog: true,
+    }],
   ],
   coverageThreshold: {
     global: {
       branches: 80,
       functions: 80,
       lines: 80,
-      statements: 80
-    }
-  }
+      statements: 80,
+    },
+  },
 };
